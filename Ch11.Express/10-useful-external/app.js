a019a4b3d42c80fe01561ba35f19a15df88a8617
dkmqflx <dkmqflx@gmail.com>
2024-05-30 15:25:00 +0900

    11.13 유용한 외부 미들웨어들

--- Ch11.Express/10-useful-external/app.js
+++ Ch11.Express/10-useful-external/app.js
@@ -24,9 +24,15 @@ const corsOptions = {
 // 라이브러리 사용해서 cors 해결한다
 app.use(cors(corsOptions));
 
+// request 안에 있는 쿠키를 보기 위한 라이브러리
 app.use(cookieParser()); // http://expressjs.com/en/resources/middleware/cookie-parser.html
+
+// 사용자한테 요청을 받을 때 마다 무엇을 요청받았고 얼마나 걸렸는지와 같은 정보를 자동으로 로그로 남기고 싶을 때 사용한다
+// 어떤 포맷으로 로그를 남길건지 인자로 전달해준다
 app.use(morgan("common")); // http://expressjs.com/en/resources/middleware/morgan.html
 
+// 공통적으로 보안에 필요한 헤더들을 추가해준다
+// response header에서 확인할 수 있다.
 app.use(helmet()); // https://github.com/helmetjs/helmet
 
 app.get("/", (req, res) => {
