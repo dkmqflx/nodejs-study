37a444faa6fe50a66c466797b106c53f0903599b
Dohyun Kim <dkmqflx@gmail.com>
2024-08-21 16:03:04 +0900

    Update 1-http.js

--- Ch09.Node로 서버 만들기/1-http.js
+++ Ch09.Node로 서버 만들기/1-http.js
@@ -1,6 +1,8 @@
 const http = require("http"); // 서버 만들기 위해서는 노드에서 제공하는 http 모듈을 사용해야 한다
 const fs = require("fs");
-// const http2 = require('http2'); // http2도 사용할 수 있다. http2 는 https와 함께 적용이 된다.
+// const http2 = require('http2'); // http2도 사용할 수 있다. http2 는 모든 브라우저에서 https와 함께 적용이 된다.
+// 개발 pc에는 https를 위한 SSL certificate가 없다. 물론 받아서 브라우저에 인증을 하면 되지만 복잡할 수 있으므로
+// 개발할 때는 http 사용하고 배포할 때 https 사용한다
 
 console.log(http.STATUS_CODES);
 console.log(http.METHODS);
