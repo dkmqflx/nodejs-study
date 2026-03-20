2b7fb89f4727b920cc46a5ff952d41bfe5333775
dkmqflx <dkmqflx@gmail.com>
2026-03-20 15:08:33 +0900

    16.6 가입, 로그인 Rest APIs 구현하기

--- CH16.Dwitter - 로그인 적용/server/controller/auth.js
+++ CH16.Dwitter - 로그인 적용/server/controller/auth.js
@@ -1,11 +1,12 @@
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import {} from 'express-async-errors';
-import * as userRepository from '../data/auth.js';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import {} from "express-async-errors";
+import * as userRepository from "../data/auth.js";
 
 // TODO: Make it secure!
-const jwtSecretKey = 'F2dN7x8HVzBWaQuEEDnhsvHXRWqAR63z';
-const jwtExpiresInDays = '2d';
+// 서버코드에서 이런 것 같고 있는것 좋지 않기 때문에 이어지는 configuration 강의에서 수정한다
+const jwtSecretKey = "F2dN7x8HVzBWaQuEEDnhsvHXRWqAR63z";
+const jwtExpiresInDays = "2d";
 const bcryptSaltRounds = 12;
 
 export async function signup(req, res) {
@@ -30,11 +31,13 @@ export async function login(req, res) {
   const { username, password } = req.body;
   const user = await userRepository.findByUsername(username);
   if (!user) {
-    return res.status(401).json({ message: 'Invalid user or password' });
+    return res.status(401).json({ message: "Invalid user or password" });
+    // 보안과 관련되어 있기 때문에 메세지를 친절하게 보내주지 않는다
   }
   const isValidPassword = await bcrypt.compare(password, user.password);
   if (!isValidPassword) {
-    return res.status(401).json({ message: 'Invalid user or password' });
+    return res.status(401).json({ message: "Invalid user or password" });
+    // 보안과 관련되어 있기 때문에 메세지를 친절하게 보내주지 않는다
   }
   const token = createJwtToken(user.id);
   res.status(200).json({ token, username });
@@ -47,7 +50,7 @@ function createJwtToken(id) {
 export async function me(req, res, next) {
   const user = await userRepository.findById(req.userId);
   if (!user) {
-    return res.status(404).json({ message: 'User not found' });
+    return res.status(404).json({ message: "User not found" });
   }
   res.status(200).json({ token: req.token, username: user.username });
 }
--- CH16.Dwitter - 로그인 적용/server/router/auth.js
+++ CH16.Dwitter - 로그인 적용/server/router/auth.js
@@ -1,38 +1,39 @@
-import express from 'express';
-import {} from 'express-async-errors';
-import { body } from 'express-validator';
-import { validate } from '../middleware/validator.js';
-import * as authController from '../controller/auth.js';
-import { isAuth } from '../middleware/auth.js';
+import express from "express";
+import {} from "express-async-errors";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.js";
+import * as authController from "../controller/auth.js";
+import { isAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
+// 라우터에서 유효성 검사
 const validateCredential = [
-  body('username')
+  body("username")
     .trim()
     .notEmpty()
-    .withMessage('username should be at least 5 characters'),
-  body('password')
+    .withMessage("username should be at least 5 characters"),
+  body("password")
     .trim()
     .isLength({ min: 5 })
-    .withMessage('password should be at least 5 characters'),
+    .withMessage("password should be at least 5 characters"),
   validate,
 ];
 
 const validateSignup = [
   ...validateCredential,
-  body('name').notEmpty().withMessage('name is missing'),
-  body('email').isEmail().normalizeEmail().withMessage('invalid email'),
-  body('url')
+  body("name").notEmpty().withMessage("name is missing"),
+  body("email").isEmail().normalizeEmail().withMessage("invalid email"),
+  body("url")
     .isURL()
-    .withMessage('invalid URL')
-    .optional({ nullable: true, checkFalsy: true }),
+    .withMessage("invalid URL")
+    .optional({ nullable: true, checkFalsy: true }), // url은 필수가 아니다. nullable: true, checkFalsy: true는 url이 없어도 된다는 의미
   validate,
 ];
-router.post('/signup', validateSignup, authController.signup);
+router.post("/signup", validateSignup, authController.signup);
 
-router.post('/login', validateCredential, authController.login);
+router.post("/login", validateCredential, authController.login);
 
-router.get('/me', isAuth, authController.me);
+router.get("/me", isAuth, authController.me);
 
 export default router;
