2707ac8b6ee947f5724ea76617fac9a62db8f3e3
dkmqflx <dkmqflx@gmail.com>
2026-03-20 16:02:13 +0900

    16.10 리액트 프론트엔드 가입/로그인 구현 💡

--- CH16.Dwitter - 로그인 적용/client/src/db/token.js
+++ CH16.Dwitter - 로그인 적용/client/src/db/token.js
@@ -1,5 +1,7 @@
-const TOKEN = 'token';
+const TOKEN = "token";
 
+// 브라우저 스토리지에 저장하는 것은 안전하지 않다
+// 다만 기본 백엔드 과정을 넘어서기 때문에 이후에 보너스 챕터에서 알려줄 것
 export default class TokenStorage {
   saveToken(token) {
     localStorage.setItem(TOKEN, token);
