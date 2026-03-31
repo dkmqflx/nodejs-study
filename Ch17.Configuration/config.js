83d9b4fff4ab502ea9685ed36a74bd1b8c89881d
dkmqflx <dkmqflx@gmail.com>
2026-03-31 16:11:12 +0900

    17.4 리팩토링 - 재사용성, 안정성 높이기 💡

--- Ch17.Configuration/config.js
+++ Ch17.Configuration/config.js
@@ -1,6 +1,7 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv"; // 환경변수 관리 라이브러리
 dotenv.config();
 
+// 환경변수가 없을 경우 기본값 설정
 function required(key, defaultValue = undefined) {
   const value = process.env[key] || defaultValue;
   if (value == null) {
@@ -11,13 +12,14 @@ function required(key, defaultValue = undefined) {
 
 export const config = {
   jwt: {
-    secretKey: required('JWT_SECRET'),
-    expiresInSec: parseInt(required('JWT_EXPIRES_SEC', 86400)),
+    secretKey: required("JWT_SECRET"),
+    expiresInSec: parseInt(required("JWT_EXPIRES_SEC", 86400)),
   },
   bcrypt: {
-    saltRounds: parseInt(required('BCRYPT_SALT_ROUNDS', 12)),
+    saltRounds: parseInt(required("BCRYPT_SALT_ROUNDS", 12)),
   },
   host: {
-    port: parseInt(required('HOST_PORT', 8080)),
+    port: parseInt(required("HOST_PORT", 8080)),
+    // 숫자로 관리해야 하는 값들은 parseInt 함수를 사용해서 숫자로 변환
   },
 };
