37e6d5cd0e0ebff366ca4be545fa37daaa77f228
dkmqflx <dkmqflx@gmail.com>
2026-04-01 21:57:44 +0900

    20.5 드위터 Auth 관련 MySQL 사용하기 💡

--- Ch20.MySQL/data/auth.js
+++ Ch20.MySQL/data/auth.js
@@ -1,22 +1,26 @@
-import { db } from '../db/database.js';
+import { db } from "../db/database.js";
 
 export async function findByUsername(username) {
+  // query 실행
   return db
-    .execute('SELECT * FROM users WHERE username=?', [username]) //
+    .execute("SELECT * FROM users WHERE username=?", [username]) //
     .then((result) => result[0][0]);
 }
 
 export async function findById(id) {
   return db
-    .execute('SELECT * FROM users WHERE id=?', [id]) //
+    .execute("SELECT * FROM users WHERE id=?", [id]) //
     .then((result) => result[0][0]);
 }
 
 export async function createUser(user) {
   const { username, password, name, email, url } = user;
+
+  // ?에 해당하는 값을 배열로 전달
+  // id는 데이터베이스에서 자동으로 생성되는 값
   return db
     .execute(
-      'INSERT INTO users (username, password, name, email, url) VALUES (?,?,?,?,?)',
+      "INSERT INTO users (username, password, name, email, url) VALUES (?,?,?,?,?)",
       [username, password, name, email, url]
     )
     .then((result) => result[0].insertId);
