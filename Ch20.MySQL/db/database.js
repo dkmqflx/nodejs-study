e384f84e2786d18db3f10751460ccdff89e34d1a
dkmqflx <dkmqflx@gmail.com>
2026-04-01 21:17:04 +0900

    20.4 노드 서버에서 연결하기

--- Ch20.MySQL/db/database.js
+++ Ch20.MySQL/db/database.js
@@ -1,6 +1,7 @@
-import mysql from 'mysql2';
-import { config } from '../config.js';
+import mysql from "mysql2"; // mysql 사용할 수 있는 라이브러리 설치
+import { config } from "../config.js";
 
+// createPool를 통해서 데이터베이스 연결을 관리하는 풀을 생성
 const pool = mysql.createPool({
   host: config.db.host,
   user: config.db.user,
@@ -8,4 +9,4 @@ const pool = mysql.createPool({
   password: config.db.password,
 });
 
-export const db = pool.promise();
+export const db = pool.promise(); // 비동기적으로 promise 방식으로 데이터베이스 연결을 관리하는 풀을 생성
