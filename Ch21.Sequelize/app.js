afb17c24fab898832c1cf3c43650bf57b42b321d
dkmqflx <dkmqflx@gmail.com>
2026-04-02 16:24:47 +0900

    21.3 Sequelize 드위터에 적용하기- 연결 💡

--- Ch21.Sequelize/app.js
+++ Ch21.Sequelize/app.js
@@ -1,23 +1,23 @@
-import express from 'express';
-import 'express-async-errors';
-import cors from 'cors';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import tweetsRouter from './router/tweets.js';
-import authRouter from './router/auth.js';
-import { config } from './config.js';
-import { initSocket } from './connection/socket.js';
-import { sequelize } from './db/database.js';
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import tweetsRouter from "./router/tweets.js";
+import authRouter from "./router/auth.js";
+import { config } from "./config.js";
+import { initSocket } from "./connection/socket.js";
+import { sequelize } from "./db/database.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
-app.use('/tweets', tweetsRouter);
-app.use('/auth', authRouter);
+app.use("/tweets", tweetsRouter);
+app.use("/auth", authRouter);
 
 app.use((req, res, next) => {
   res.sendStatus(404);
@@ -28,6 +28,8 @@ app.use((error, req, res, next) => {
   res.sendStatus(500);
 });
 
+// sequelize.sync()를 통해서 데이터베이스 연결을 관리하는 풀을 생성
+// 만약 데이터베이스가 존재하지 않으면 데이터베이스를 생성하는 역할 등을 한다
 sequelize.sync().then(() => {
   const server = app.listen(config.host.port);
   initSocket(server);
--- Ch21.Sequelize/db/database.js
+++ Ch21.Sequelize/db/database.js
@@ -1,9 +1,9 @@
-import { config } from '../config.js';
-import SQ from 'sequelize';
+import { config } from "../config.js";
+import SQ from "sequelize"; // sequelize 사용할 수 있는 라이브러리 설치
 
 const { host, user, database, password } = config.db;
 export const sequelize = new SQ.Sequelize(database, user, password, {
   host,
-  dialect: 'mysql',
+  dialect: "mysql",
   logging: false,
 });
