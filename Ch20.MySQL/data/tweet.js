fd2da10b474fd83e372c17a075d9defcee3b4fe8
dkmqflx <dkmqflx@gmail.com>
2026-04-02 11:54:54 +0900

    20.6 드위터 트윗 관련 MySQL 사용하기 💡

--- Ch20.MySQL/data/tweet.js
+++ Ch20.MySQL/data/tweet.js
@@ -1,8 +1,12 @@
-import { db } from '../db/database.js';
+import { db } from "../db/database.js";
 
+// tweets as tw - tweets 테이블을 tw로 별칭 붙이기
+// users as us - users 테이블을 us로 별칭 붙이기
+// tw.userId=us.id - tweets 테이블의 userId와 users 테이블의 id를 조인
+// ORDER BY tw.createdAt DESC - tweets 테이블의 createdAt 컬럼을 내림차순으로 정렬
 const SELECT_JOIN =
-  'SELECT tw.id, tw.text, tw.createdAt, tw.userId, us.username, us.name, us.url FROM tweets as tw JOIN users as us ON tw.userId=us.id';
-const ORDER_DESC = 'ORDER BY tw.createdAt DESC';
+  "SELECT tw.id, tw.text, tw.createdAt, tw.userId, us.username, us.name, us.url FROM tweets as tw JOIN users as us ON tw.userId=us.id";
+const ORDER_DESC = "ORDER BY tw.createdAt DESC";
 
 export async function getAll() {
   return db
@@ -24,7 +28,7 @@ export async function getById(id) {
 
 export async function create(text, userId) {
   return db
-    .execute('INSERT INTO tweets (text, createdAt, userId) VALUES(?,?,?)', [
+    .execute("INSERT INTO tweets (text, createdAt, userId) VALUES(?,?,?)", [
       text,
       new Date(),
       userId,
@@ -34,10 +38,10 @@ export async function create(text, userId) {
 
 export async function update(id, text) {
   return db
-    .execute('UPDATE tweets SET text=? WHERE id=?', [text, id])
+    .execute("UPDATE tweets SET text=? WHERE id=?", [text, id])
     .then(() => getById(id));
 }
 
 export async function remove(id) {
-  return db.execute('DELETE FROM tweets WHERE id=?', [id]);
+  return db.execute("DELETE FROM tweets WHERE id=?", [id]);
 }
