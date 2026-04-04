161a3c5d28a148c2ffd96eecc330f9d337cc2cb0
dkmqflx <dkmqflx@gmail.com>
2026-04-04 16:59:18 +0900

    21.5 Sequelize 드위터에 적용하기- 트윗 💡

--- Ch21.Sequelize/data/tweet.js
+++ Ch21.Sequelize/data/tweet.js
@@ -1,10 +1,10 @@
-import SQ from 'sequelize';
-import { sequelize } from '../db/database.js';
-import { User } from './auth.js';
+import SQ from "sequelize";
+import { sequelize } from "../db/database.js";
+import { User } from "./auth.js";
 const DataTypes = SQ.DataTypes;
 const Sequelize = SQ.Sequelize;
 
-const Tweet = sequelize.define('tweet', {
+const Tweet = sequelize.define("tweet", {
   id: {
     type: DataTypes.INTEGER,
     autoIncrement: true,
@@ -16,28 +16,31 @@ const Tweet = sequelize.define('tweet', {
     allowNull: false,
   },
 });
-Tweet.belongsTo(User);
+
+Tweet.belongsTo(User); // Tweet 테이블과 User 테이블을 조인
 
 const INCLUDE_USER = {
   attributes: [
-    'id',
-    'text',
-    'createdAt',
-    'userId',
-    [Sequelize.col('user.name'), 'name'],
-    [Sequelize.col('user.username'), 'username'],
-    [Sequelize.col('user.url'), 'url'],
+    "id",
+    "text",
+    "createdAt",
+    "userId",
+    [Sequelize.col("user.name"), "name"], // user 테이블의 name 컬럼을 name으로 별칭 붙이기
+    [Sequelize.col("user.username"), "username"], // user 테이블의 username 컬럼을 username으로 별칭 붙이기
+    [Sequelize.col("user.url"), "url"], // user 테이블의 url 컬럼을 url으로 별칭 붙이기
   ],
   include: {
-    model: User,
+    model: User, // User 테이블을 조인
     attributes: [],
   },
 };
 
 const ORDER_DESC = {
-  order: [['createdAt', 'DESC']],
+  // createdAt 컬럼을 내림차순으로 정렬
+  order: [["createdAt", "DESC"]],
 };
 
+// 모든 트윗을 조회하는 함수, INCLUDE_USER와 ORDER_DESC를 합쳐서 조회
 export async function getAll() {
   return Tweet.findAll({ ...INCLUDE_USER, ...ORDER_DESC });
 }
@@ -47,25 +50,27 @@ export async function getAllByUsername(username) {
     ...INCLUDE_USER,
     ...ORDER_DESC,
     include: {
-      ...INCLUDE_USER.include,
-      where: { username },
+      ...INCLUDE_USER.include, // 기존의 INCLUDE_USER.include를 사용하면서
+      where: { username }, // 아래 조건을 추가
     },
   });
 }
 
 export async function getById(id) {
   return Tweet.findOne({
-    where: { id },
+    where: { id }, // id 컬럼이 id와 일치하는 트윗을 조회
     ...INCLUDE_USER,
   });
 }
 
 export async function create(text, userId) {
+  // text와 userId를 사용하여 트윗을 생성
   return Tweet.create({ text, userId }) //
     .then((data) => this.getById(data.dataValues.id));
 }
 
 export async function update(id, text) {
+  // id와 text를 사용하여 트윗을 업데이트
   return Tweet.findByPk(id, INCLUDE_USER) //
     .then((tweet) => {
       tweet.text = text;
@@ -74,6 +79,7 @@ export async function update(id, text) {
 }
 
 export async function remove(id) {
+  // id를 사용하여 트윗을 삭제
   return Tweet.findByPk(id) //
     .then((tweet) => {
       tweet.destroy();
