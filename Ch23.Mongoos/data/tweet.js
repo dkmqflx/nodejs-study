c7d347f108f95f265480fa3b06379c109ca7ee11
dkmqflx <dkmqflx@gmail.com>
2026-04-07 15:13:16 +0900

    23.5 Mongoose 드위터에 적용하기- 트윗 💡

--- Ch23.Mongoos/data/tweet.js
+++ Ch23.Mongoos/data/tweet.js
@@ -1,7 +1,8 @@
-import Mongoose from 'mongoose';
-import { useVirtualId } from '../database/database.js';
-import * as UserRepository from './auth.js';
+import Mongoose from "mongoose";
+import { useVirtualId } from "../database/database.js";
+import * as UserRepository from "./auth.js";
 
+// 트윗 스키마
 const tweetSchema = new Mongoose.Schema(
   {
     text: { type: String, required: true },
@@ -14,20 +15,24 @@ const tweetSchema = new Mongoose.Schema(
 );
 
 useVirtualId(tweetSchema);
-const Tweet = Mongoose.model('Tweet', tweetSchema);
+const Tweet = Mongoose.model("Tweet", tweetSchema);
 
+// 모든 트윗 조회
 export async function getAll() {
   return Tweet.find().sort({ createdAt: -1 });
 }
 
+// 사용자 이름으로 트윗 조회
 export async function getAllByUsername(username) {
   return Tweet.find({ username }).sort({ createdAt: -1 });
 }
 
+// 트윗 ID로 트윗 조회
 export async function getById(id) {
   return Tweet.findById(id);
 }
 
+// 트윗 생성
 export async function create(text, userId) {
   return UserRepository.findById(userId).then((user) =>
     new Tweet({
@@ -39,10 +44,12 @@ export async function create(text, userId) {
   );
 }
 
+// 트윗 업데이트
 export async function update(id, text) {
   return Tweet.findByIdAndUpdate(id, { text }, { returnOriginal: false });
 }
 
+// 트윗 삭제
 export async function remove(id) {
   return Tweet.findByIdAndDelete(id);
 }
