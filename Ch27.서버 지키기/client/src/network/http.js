d21102f25a001eb37b890a256e05b48681a99795
dkmqflx <dkmqflx@gmail.com>
2026-04-28 17:16:03 +0900

    27.7 프론트엔드 - Axios 사용하기 💡

--- Ch27.서버 지키기/client/src/network/http.js
+++ Ch27.서버 지키기/client/src/network/http.js
@@ -1,11 +1,12 @@
-import axios from 'axios';
-import axiosRetry from 'axios-retry';
+import axios from "axios";
+import axiosRetry from "axios-retry";
 
 const defaultRetryConfig = {
   retries: 5,
   initialDelayMs: 100,
 };
 
+// https://github.com/axios/axios
 export default class HttpClient {
   constructor(
     baseURL,
@@ -15,11 +16,16 @@ export default class HttpClient {
   ) {
     this.authErrorEventBus = authErrorEventBus;
     this.getCsrfToken = getCsrfToken;
+
+    // axios 인스턴스 생성
     this.client = axios.create({
       baseURL: baseURL,
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
       withCredentials: true,
     });
+
+    // axios-retry 설정
+    // https://github.com/softonic/axios-retry
     axiosRetry(this.client, {
       retries: config.retries,
       retryDelay: (retry) => {
@@ -27,6 +33,8 @@ export default class HttpClient {
         const jitter = delay * 0.1 * Math.random(); // 10, 20, .... 160
         return delay + jitter;
       },
+
+      // 429 에러가 발생하면 재시도
       retryCondition: (err) =>
         axiosRetry.isNetworkOrIdempotentRequestError(err) ||
         err.response.status === 429,
@@ -40,7 +48,7 @@ export default class HttpClient {
       method,
       headers: {
         ...headers,
-        '_csrf-token': this.getCsrfToken(),
+        "_csrf-token": this.getCsrfToken(),
       },
       data: body,
     };
@@ -49,13 +57,15 @@ export default class HttpClient {
       const res = await this.client(req);
       return res.data;
     } catch (err) {
+      // 에러 처리
       if (err.response) {
         const data = err.response.data;
+
         const message =
-          data && data.message ? data.message : 'Something went wrong! 🤪';
+          data && data.message ? data.message : "Something went wrong! 🤪";
         throw new Error(message);
       }
-      throw new Error('connection error');
+      throw new Error("connection error");
     }
   }
 }
