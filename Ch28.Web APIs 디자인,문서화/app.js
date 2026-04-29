079873d8d53729ec666140f5cc7b06b610d6918d
dkmqflx <dkmqflx@gmail.com>
2026-04-29 16:39:11 +0900

    28.7 드위터 - 문서 호스팅 하기 💡

--- Ch28.Web APIs 디자인,문서화/app.js
+++ Ch28.Web APIs 디자인,문서화/app.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import 'express-async-errors';
-import cors from 'cors';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import tweetsRouter from './router/tweets.js';
-import authRouter from './router/auth.js';
-import { config } from './config.js';
-import { initSocket } from './connection/socket.js';
-import { sequelize } from './db/database.js';
-import { authHandler } from './middleware/auth.js';
-import yaml from 'yamljs';
-import swaggerUI from 'swagger-ui-express';
-import * as OpenAPIValidator from 'express-openapi-validator';
-import * as apis from './controller/index.js';
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import tweetsRouter from "./router/tweets.js";
+import authRouter from "./router/auth.js";
+import { config } from "./config.js";
+import { initSocket } from "./connection/socket.js";
+import { sequelize } from "./db/database.js";
+import { authHandler } from "./middleware/auth.js";
+import yaml from "yamljs";
+import swaggerUI from "swagger-ui-express";
+import * as OpenAPIValidator from "express-openapi-validator";
+import * as apis from "./controller/index.js";
 
 const app = express();
 
@@ -20,19 +20,26 @@ const corsOption = {
   origin: config.cors.allowedOrigin,
   optionsSuccessStatus: 200,
 };
-const openAPIDocument = yaml.load('./api/openapi.yaml');
+
+// yaml 형태로 작성한 문서를 로드한다
+const openAPIDocument = yaml.load("./api/openapi.yaml");
 
 app.use(express.json());
 app.use(helmet());
 app.use(cors(corsOption));
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(openAPIDocument));
-app.use('/tweets', tweetsRouter);
-app.use('/auth', authRouter);
+// OpenAPI 문서 표시
+// npm install swagger-ui-express yamljs express-openapi-validator
+// /api-docs 경로로 접속하면 OpenAPI 문서를 표시한다
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(openAPIDocument));
+app.use("/tweets", tweetsRouter);
+app.use("/auth", authRouter);
+
+// OpenAPI 문서 검증을 위해 사용한다
 app.use(
   OpenAPIValidator.middleware({
-    apiSpec: './api/openapi.yaml',
+    apiSpec: "./api/openapi.yaml",
     validateResponses: true,
 
     operationHandlers: {
